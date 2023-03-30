c71c89699803a8ccad6ffe2195512c6cb5ad1967
nils-n <52067471+nils-n@users.noreply.github.com>
2023-03-30 16:34:09 +0000

    add more tests for d3 properties

--- assets/javascript/diagram.js
+++ assets/javascript/diagram.js
@@ -20,22 +20,62 @@ const diagram = {
     },
 }
 
-// from here https://busypeoples.github.io/post/testing-d3-with-jasmine/
-function barChart() {
+// based on https://busypeoples.github.io/post/testing-d3-with-jasmine/
+function animateExample() {
+
     var that = {};
 
     that.render = function() {
-       var svg = d3.select('#main-diagram').append('svg')
-             .attr('height', '500')
-             .attr('width', '500')
-            .append('g')
-             .attr("transform", "translate(0, 0)");
+        const margin = {
+            top: 20,
+            right: 20,
+            bottom: 50,
+            left: 20
+        }
+        const height = 200;
+        const width = 300;
 
+        // now create a circle and move its position 
+        const circles = [{
+            x: 2,
+            y: 2,
+            color: "blue"
+        }, {
+            x: 70,
+            y: 40,
+            color: "lightblue"
+        }, {
+            x: 200,
+            y: 140,
+            color: "yellowgreen"
+        }]
+
+        const svg = d3.select("#main-diagram")
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.bottom + margin.top)
+        .append('g')
+        .attr('transform', `translate( ${margin.left} , ${margin.top} )`)
+    
+        svg.selectAll("mycircles")
+        .data(circles)
+        .enter()
+        .append('circle')
+        .attr('cx', function (d) {
+            return d.x
+        })
+        .attr('cy', function (d) {
+            return d.y
+        })
+        .attr("r", 40)
+        .attr('fill', function (d) {
+            return d.color
+        })
     };
-
+    
     return that;
 }
 
 module.exports = {
-    diagram, barChart
+    diagram, animateExample
 }
--- test/diagram.test.js
+++ test/diagram.test.js
@@ -3,49 +3,52 @@
  */
 const d3 = require('d3');
 
-const { diagram, barChart }  = require('../assets/javascript/diagram')
+const {
+  diagram,
+  animateExample
+} = require('../assets/javascript/diagram')
 
 
-beforeAll( () => {
-    let fs = require('fs')
-    let fileContents = fs.readFileSync('index.html', 'utf-8')
-    document.open()
-    document.write(fileContents)
-    document.close()
+beforeAll(() => {
+  let fs = require('fs')
+  let fileContents = fs.readFileSync('index.html', 'utf-8')
+  document.open()
+  document.write(fileContents)
+  document.close()
 })
 
 describe('diagram objects has correct keys', () => {
-    test('sticks key exists', () => {
-        expect('sticks' in diagram).toBe(true);
-    })
+  test('sticks key exists', () => {
+    expect('sticks' in diagram).toBe(true);
+  })
 })
 
 describe('diagram object works properly', () => {
-    beforeAll( () => {
-        diagram.initDiagram()
-        diagram.sticks.push('something')
-        diagram.resetSticks()
-    })
-    test('resetSticks should empty the sticks array', () => {
-        expect( diagram.sticks.length).toBe(0);
-    })
-    test('button click of "Reset" Button should clear stick array', () => {
-        diagram.sticks.push('something');
-        resetButton = document.getElementById('hero-reset').click();
-        expect(diagram.sticks.length).toEqual(0);
-    })
-    test('stick array should be non-zero after "Throw" button press', () => {
-        throwButton = document.getElementById('hero-throw').click();
-        expect(diagram.sticks.length).toEqual(1);
-    })
+  beforeAll(() => {
+    diagram.initDiagram()
+    diagram.sticks.push('something')
+    diagram.resetSticks()
+  })
+  test('resetSticks should empty the sticks array', () => {
+    expect(diagram.sticks.length).toBe(0);
+  })
+  test('button click of "Reset" Button should clear stick array', () => {
+    diagram.sticks.push('something');
+    resetButton = document.getElementById('hero-reset').click();
+    expect(diagram.sticks.length).toEqual(0);
+  })
+  test('stick array should be non-zero after "Throw" button press', () => {
+    throwButton = document.getElementById('hero-throw').click();
+    expect(diagram.sticks.length).toEqual(1);
+  })
 })
 
 describe("test basic function of d3 library", () => {
-  beforeAll( () => {
+  beforeAll(() => {
     const svg = d3.select("#main-diagram")
-    .append('svg')
-    .attr('width', 500)
-    .attr('height',300)
+      .append('svg')
+      .attr('width', 500)
+      .attr('height', 300)
   })
   test("d3 should exist", () => {
     expect(d3).toBeDefined();
@@ -54,42 +57,26 @@ describe("test basic function of d3 library", () => {
     const div = document.getElementById('main-diagram')
     expect(div.hasChildNodes()).toBe(true);
   });
+})
 
-
-  describe('Test D3.js with jasmine ', function() {
-    var c;
-  
-    beforeEach(function() {
-      c = barChart();
-      c.render();
-    });
-  
-    afterEach(function() {
-      d3.selectAll('svg').remove();
-    });
-  
-    describe('the svg' ,function() {
-      it('should be created', function() {
-          expect(getSvg()).not.toBeNull();
-      });
-  
-      it('should have the correct height', function() {
-        expect(getSvg().attr('width')).toBe('500');
-      });
-  
-      it('should have the correct width', function() {
-        expect(getSvg().attr('width')).toBe('500');
-      });
-    });
-  
-    function getSvg() {
-      return d3.select('svg');
-    }
-  
-  });
-
-
-
-
+// taken from  https://busypeoples.github.io/post/testing-d3-with-jasmine/
+describe('d3 should draw an svg in the main diagram div ', function () {
+var c;
+beforeEach(function () {
+  c = animateExample();
+  c.render();
+});
+afterEach(function () {
+  d3.selectAll('svg').remove();
+});
+test('an svg element should exist', () => {
+  expect(d3.select('svg')).not.toBeNull();
+})
+test('the svg element should have a width of 340', () => {
+  expect(d3.select('svg').attr('width')).toBe('340');
+})
+test('the svg element should have a height of 270', () => {
+  expect(d3.select('svg').attr('height')).toBe('270');
+})
 
 });
