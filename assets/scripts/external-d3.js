2b1bce66a8218efe5b6e50f993ab45f89b44f6d0
Nils <nils@nilsn-mac.psy.gla.ac.uk>
2023-04-06 20:35:22 +0100

    refactor into functions

--- assets/scripts/external-d3.js
+++ assets/scripts/external-d3.js
@@ -7,67 +7,96 @@ const sticks = [
     { "position": 0.15, "orientation": 45 } ,
  ];
 
- console.log(sticks)
 
- // this will be variable too later but for testing I will hard-code it.
+// this will be variable too later but for testing I will hard-code it.
 const diagram = document.getElementById('main-diagram')
 const height = diagram.clientHeight;
 const width = diagram.clientWidth;
 
-const margin = {
-    top: 20,
-    right: 20,
-    bottom: 50,
-    left: 20
-}
+// now call the functions to draw the elements 
+console.log(sticks)
+svg = drawEmptyDiagram( width, height )
+drawMidlines( svg )
+drawSticks( svg,  sticks )
 
 
-// create a scale for the input data 
-const xScale = d3.scaleLinear() 
-.domain([0,1])
-.range([0, width])
- 
-// now create a circle and move its position 
-const dataset = [{
-    x: sticks[0].position,
-    y: 100,
-    color: "blue"
-}, {
-    x: sticks[1].position,
-    y: 100,
-    color: "lightblue"
-}, {
-    x: sticks[2].position,
-    y: 100,
-    color: "yellowgreen"
-}]
+/**
+ * This function draws an empty svg element on the DOM, returns d3 SVG element
+ */
+function drawEmptyDiagram( width , height) {
 
+    // also hard coded for now - this should evenutally become variable, too.
+    const margin = {
+        top: 20,
+        right: 20,
+        bottom: 50,
+        left: 20
+    }
 
-console.log(dataset)
-
-const svg = d3.select("#main-diagram")
+    // create empty svg
+    const svg = d3.select("#main-diagram")
     .append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.bottom + margin.top)
     .append('g')
     .attr('transform', `translate( ${margin.left} , ${margin.top} )`)
 
-svg.selectAll("test-sticks")
-    .data(dataset)
-    .enter()
-    .append('circle')
-    .attr('cx', function (d) {
-        return xScale(d.x)
-    })
-    .attr('cy', function (d) {
-        return d.y
-    })
-    .attr("r", 40)
-    .attr('fill', function (d) {
-        return d.color
-    })
+    // make the background transparent 
+    svg.fillStyle = "#FF0000";
 
+    return svg
+}
 
-// make the background transparent 
-svg.fillStyle = "#FF0000";
+/**
+ *  This function will eventually draw all the sticks on the canvas 
+ */
 
+ function drawSticks( svg, sticks ) {
+   
+       // create a scale for the input data 
+       const xScale = d3.scaleLinear() 
+       .domain([0,1])
+       .range([0, width])
+
+    // now create a circle and move its position 
+    const dataset = [{
+        x: sticks[0].position,
+        y: 100,
+        color: "blue"
+    }, {
+        x: sticks[1].position,
+        y: 100,
+        color: "lightblue"
+    }, {
+        x: sticks[2].position,
+        y: 100,
+        color: "yellowgreen"
+    }]
+
+    console.log(dataset)
+
+    svg.selectAll("test-sticks")
+        .data(dataset)
+        .enter()
+        .append('circle')
+        .attr('cx', function (d) {
+             console.log(` x is ${d.x} but scaled it is ${xScale(d.x)}`)
+            return xScale(d.x)
+        })
+        .attr('cy', function (d) {
+            return d.y
+        })
+        .attr("r", 40)
+        .attr('fill', function (d) {
+            return d.color
+        })
+ }
+ 
+/**
+ * this is a function to visualize the parallel midlines.
+ */
+function drawMidlines() {
+
+    console.log('entering function : drawMidlines ')
+
+}
