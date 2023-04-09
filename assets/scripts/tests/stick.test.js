e14fadc86b4eeb2c8b64c928ebb810f55a589f48
Nils <52067471+nils-n@users.noreply.github.com>
2023-04-09 19:41:01 +0100

    fix syntax of unit test for Stick class to pass jshint

--- assets/scripts/tests/stick.test.js
+++ assets/scripts/tests/stick.test.js
@@ -20,21 +20,21 @@ describe( `Class ${Stick.name}`, () => {
     beforeEach( () => {
         const data = { position: '', orientation:'' ,  length: '' , colour: '' }; 
         model = new Stick(data);
-    })
+    });
 
     test("exists and is initialized with empty values", () => {
           expect(model).toBeDefined();
-          expect(model.position).toBeDefined()
-          expect(model.orientation).toBeDefined()
-          expect(model.length).toBeDefined()
-          expect(model.colour).toBeDefined()
-          expect(model.sector).toBeNull()
-          expect(model.verticalOffsetOnScreen).toBeNull()
+          expect(model.position).toBeDefined();
+          expect(model.orientation).toBeDefined();
+          expect(model.length).toBeDefined();
+          expect(model.colour).toBeDefined();
+          expect(model.sector).toBeNull();
+          expect(model.verticalOffsetOnScreen).toBeNull();
         });
 
     test('maps a position from other sectors correctly into the first sector', () => {
        // random stick in the 4th sector 
-       const randomPosition = 4.5
+       const randomPosition = 4.5;
        const minAllowedPosition = 0.0;
        const maxAllowedPosition = 1.0;
 
@@ -42,7 +42,7 @@ describe( `Class ${Stick.name}`, () => {
 
        expect(  result ).toBeLessThanOrEqual( maxAllowedPosition );
        expect(  result ).toBeGreaterThanOrEqual( minAllowedPosition);
-    }) 
+    }) ;
 
     test('handles correctly the case that mapped number is between 0 and 1', () => {
         // stick in the 4th sector that is mapped betweeon 0 and 1
@@ -52,7 +52,7 @@ describe( `Class ${Stick.name}`, () => {
         const result =  model.mapPositionIntoFirstSector( inputPosition );
 
         expect(  result ).toBe( mappedPosition );
-    })
+    });
 
     test('handles correctly the case that mapped number is between 1 and 2', () => {
         // stick in the 4th sector that is mapped betweeon 1 and 2
@@ -61,15 +61,15 @@ describe( `Class ${Stick.name}`, () => {
         const randomInputPositions = [ 5.5, 5.6, 5.7, 5.8, 5.9, 6.0] ;
         const correctlyMappedPositions = [ 0.5, 0.4, 0.3, 0.2, 0.1, 0.0 ] ;
 
-        const mappedPositions = []
+        const mappedPositions = [];
         for (const inputPosition of randomInputPositions) {
-            mappedPositions.push( model.mapPositionIntoFirstSector( inputPosition ) )
+            mappedPositions.push( model.mapPositionIntoFirstSector( inputPosition ) );
         }
 
         for (const i in correctlyMappedPositions){
-            expect( mappedPositions[i] ).toBeCloseTo( correctlyMappedPositions[i] )
+            expect( mappedPositions[i] ).toBeCloseTo( correctlyMappedPositions[i] );
         }
-    })
+    });
 
     
         
